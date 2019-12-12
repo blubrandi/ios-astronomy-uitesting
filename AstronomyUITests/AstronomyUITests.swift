2ca8f2cdae30e3f3e7cc450c8d2cbc99f3ccc9c2
Brandi <blubrandi0@gmail.com>
2019-12-12 16:56:12 -0700

    Added testBackFromDetailVC() test.  Test passes.

--- AstronomyUITests/AstronomyUITests.swift
+++ AstronomyUITests/AstronomyUITests.swift
@@ -23,13 +23,13 @@ class AstronomyUITests: XCTestCase {
     /*
      - Collection View Controller -
      Async test for photo loading ✅
-     Arrow in navbar switches sols
+     Arrow in navbar switches sols ✅
      
      - Detail View Controller -
      Tap on image from collection view controller and go to detail view controller ✅
      View corresponding image in detail view controller ✅
-     save image to photo library
-     Clicking Sol name in navbar from DetailVC takes you back to Sol Collection View VC
+     Save image to photo library ✅
+     Clicking Sol name (as back button) in navbar from DetailVC takes you back to Sol Collection View VC
      */
     
     //MARK: Tests
@@ -50,17 +50,6 @@ class AstronomyUITests: XCTestCase {
         app.collectionViews.children(matching: .cell).element(boundBy: 7).children(matching: .other).element.tap()
     }
     
-//    func testSaveImageOnFirstRunAsksForPermission() {
-//        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
-//
-//        let photodetailviewcontrollerSavebuttonButton = app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-//        photodetailviewcontrollerSavebuttonButton.tap()
-//        app.alerts["“Astronomy” Would Like to Access Your Photos"].scrollViews.otherElements.buttons["OK"].tap()
-//        photodetailviewcontrollerSavebuttonButton.tap()
-//        app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
-//
-//    }
-    
     func testSaveImage() {
         
         let app = XCUIApplication()
@@ -68,6 +57,21 @@ class AstronomyUITests: XCTestCase {
         let photodetailviewcontrollerSavebuttonButton = app/*@START_MENU_TOKEN@*/.buttons["PhotoDetailViewController.SaveButton"]/*[[".buttons[\"Save to Photo Library\"]",".buttons[\"PhotoDetailViewController.SaveButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         photodetailviewcontrollerSavebuttonButton.tap()
         app.alerts["Photo Saved!"].scrollViews.otherElements.buttons["Okay"].tap()
+        
+    }
+    
+    func testSwitchSolCollectionView() {
+        
+        app.navigationBars["Sol 15"].buttons["PhotosCollectionViewController.NextSolButton"].tap()
+        app.navigationBars["Sol 16"].buttons["PhotosCollectionViewController.NextSolButton"].tap()
+        app.navigationBars["Sol 16"].buttons["PhotosCollectionViewController.PreviousSolButton"].tap()
+        
+    }
+    
+    func testBackFromDetailVC() {
+        
+        app.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.tap()
+        app.navigationBars["Title"].buttons["Sol 15"].tap()
         
     }
     
